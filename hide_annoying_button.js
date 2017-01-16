00e048e9447bfa005f7eecf272bb4fbc864811ef
Heitor Prezia <h.prezia@gmail.com>
2017-01-16 16:14:40 -0200

    Do some stuff

---
+++ hide_annoying_button.js
@@ -0,0 +1,8 @@
+(function() {
+
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log(document.getElementById('notifications_button'));
+    document.getElementById('notifications_button').style.display = 'none';
+  });
+
+}).call(this);
